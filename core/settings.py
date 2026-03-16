eadc3732b3c5d9b32a1aa24360cb725e1b6df393
AgbogidiRhunor <roobill1234@gmail.com>
2026-03-16 23:32:51 +0100

    Switch email backend from console logging back to SMTP delivery

--- core/settings.py
+++ core/settings.py
@@ -148,14 +148,13 @@ ALLOWED_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png', 'gif', 'webp']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = config('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 DEFAULT_FROM_EMAIL = f'SIWES Logbook <{EMAIL_HOST_USER}>'
-
 EMAIL_TIMEOUT = 10
 
 SECURE_SSL_REDIRECT = False
